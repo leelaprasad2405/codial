0d55240941765e4790aa133a4107e044958aa3a7
Leela <leelaprasad2405@gmail.com>
2023-08-02 00:17:22 +0530

    successfully added and deleted the comments like posts

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -48,3 +48,53 @@ module.exports.create = async function (req, res) {
 //     });
 // }
 
+
+// module.exports.destroy = function(req, res){
+//   Comment.findById(req.params.id, function(err, comment){
+//     if(comment.user == req.user.id){
+//       let postId = comment.post;
+//       comment.remove();
+//       Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}},function(err, post){
+//         return res.redirect('back');
+//       })
+//     }else{
+//       return res.redirect('back');
+//     }
+//   });
+// }
+
+
+module.exports.destroy = async function (req, res) {
+  try {
+    let comment = await Comment.findById(req.params.id);
+
+    if (comment.user == req.user.id) {
+      // await Like.deleteMany({ likeable: post._id, onMondel: 'Post' });
+      // await Like.deleteMany({ _id: { $in: post.Comment } });
+      await Comment.deleteMany({ post: req.params.id });
+
+      // Instead of using req.flash, you can add a success message to the response locals
+      res.locals.successMessage = 'Post and associated comments deleted';
+      await comment.deleteOne();
+
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            comment: req.params.id,
+          },
+          message: 'Post deleted',
+        });
+      }
+      return res.redirect('back');
+    } else {
+      return res.redirect('back');
+    }
+  } catch (err) {
+    console.error(err);
+
+    // Instead of using req.flash, you can add an error message to the response locals
+    res.locals.errorMessage = 'Error deleting the post';
+
+    return res.redirect('back');
+  }
+};
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -12,6 +12,7 @@
 
 // from cg
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res) {
   try {
@@ -25,3 +26,52 @@ module.exports.create = async function(req, res) {
     return res.status(500).send('Internal Server Error');
   }
 }
+
+// module.exports.destroy = function(req, res){
+//   Post.findById(req.params.id, function(err, post){
+//     // .id means converting the object id into string
+//     if(post.user == req.user.id){
+//       post.remove();
+//       Comment.deleteMany({post: req.params.id}, function(err){
+//         return res.redirect('back');
+//       });
+//     }else{
+//       return res.redirect('back');
+//     }
+//   });
+// }
+
+module.exports.destroy = async function (req, res) {
+  try {
+    let post = await Post.findById(req.params.id);
+
+    if (post.user == req.user.id) {
+      // await Like.deleteMany({ likeable: post._id, onMondel: 'Post' });
+      // await Like.deleteMany({ _id: { $in: post.Comment } });
+      await Comment.deleteMany({ post: req.params.id });
+
+      // Instead of using req.flash, you can add a success message to the response locals
+      res.locals.successMessage = 'Post and associated comments deleted';
+      await post.deleteOne();
+
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            post: req.params.id,
+          },
+          message: 'Post deleted',
+        });
+      }
+      return res.redirect('back');
+    } else {
+      return res.redirect('back');
+    }
+  } catch (err) {
+    console.error(err);
+
+    // Instead of using req.flash, you can add an error message to the response locals
+    res.locals.errorMessage = 'Error deleting the post';
+
+    return res.redirect('back');
+  }
+};
