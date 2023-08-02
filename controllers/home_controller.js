df2baa29657527209fe75efb9421af97ca90a770
Leela <leelaprasad2405@gmail.com>
2023-08-03 00:03:36 +0530

    Styling through scss was applied successfully

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,58 +1,87 @@
 const Post = require('../models/post');
-module.exports.home = function(req, res){
-    // //return res.end('<h1>express is up for codial!</h1>');
-    // console.log(req.cookies);  // here we are requesting the cookie from server where we had created a cookie as example in browser (user_id 11) (can be viewed on terminal)
-    // res.cookie('user_id', 25);  // here we will get as the response on browser (can be viewed on browser)
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codial | Home"
-    //         posts: posts
-    //     });
+const User = require('../models/user');
+
+// module.exports.home = function(req, res){
+//     // //return res.end('<h1>express is up for codial!</h1>');
+//     // console.log(req.cookies);  // here we are requesting the cookie from server where we had created a cookie as example in browser (user_id 11) (can be viewed on terminal)
+//     // res.cookie('user_id', 25);  // here we will get as the response on browser (can be viewed on browser)
+//     // Post.find({}, function(err, posts){
+//     //     return res.render('home', {
+//     //         title: "Codial | Home"
+//     //         posts: posts
+//     //     });
     
-    // });
+//     // });
 
-    // to populate the user of each post (small changes from above snip)
-    // Post.find({}).populate('user').exec(function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codial | Home",
-    //         posts: posts
-    //     });
-
-
-    // });
+//     // to populate the user of each post (small changes from above snip)
+//     // Post.find({}).populate('user').exec(function(err, posts){
+//     //     return res.render('home', {
+//     //         title: "Codial | Home",
+//     //         posts: posts
+//     //     });
 
 
-   // from cg for above
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec()
-    .then(posts => {
-        return res.render('home', {
-            title: "Codial | Home",
-            posts: posts
-        });
-    })
-    .catch(err => {
-        console.log('Error:', err);
-        return res.status(500).send('Internal Server Error');
-    });
+//     // });
+
+
+//    // from cg for above
+//     Post.find({})
+//     .populate('user')
+//     .populate({
+//         path: 'comments',
+//         populate: {
+//             path: 'user'
+//         }
+//     })
+//     .exec()
+
+//     .then(posts => {
+//         return res.render('home', {
+//             title: "Codial | Home",
+//             posts: posts
+//         });
+//     })
+//     .catch(err => {
+//         console.log('Error:', err);
+//         return res.status(500).send('Internal Server Error');
+//     });
 
 
 
 
-    // return res.render('home', {
-    //     title: "Home"
-    // });
-}
+//     // return res.render('home', {
+//     //     title: "Home"
+//     // });
+// }
 
 //module.exports.actionName = function(req, res){}
 
 
 
 // cookies values (tokens) are generally encrypted in real cases just for our understanding we had done created a understandable token
+
+
+module.exports.home = async function (req, res){
+    try {
+        let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Home",
+            posts: posts,
+            all_users : users
+        });
+    } catch (error) {
+        console.log('ERROR', error);
+        return;
+    }
+}
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,11 +1,119 @@
-const User = require('../models/user')
+// const User = require('../models/user')
 
-module.exports.profile = function(req, res){
-    //res.end('<h1>User Profile!!!!</h1>');  // for home
-    return res.render('user_profile',{
-        title: "User Profile"
-    });
-}
+// module.exports.profile = function (req, res) {
+//     // return res.end('<h1>User Profile </h1>'); // for home
+
+//     // This is sending title value to user_profile.ejs file for home page
+//     User.findById(req.params.id, function (err, user) {
+//         return res.render('user_profile', {
+//             title: "Profile",
+//             profile_user: user
+//         });
+//     });
+
+// }
+
+const User = require('../models/user');
+
+module.exports.profile = async function (req, res) {
+    try {
+        // This is sending title value to user_profile.ejs file for the home page
+        const user = await User.findByIdAndUpdate(req.params.id).exec();
+
+        if (!user) {
+            // Handle the case when the user is not found
+            return res.redirect('back');
+        }
+
+        return res.render('user_profile', {
+            title: "Profile",
+            profile_user: user
+        });
+    } catch (err) {
+        console.log('Error in finding user', err);
+        return res.redirect('back');
+    }
+};
+
+// updating user details
+
+// module.exports.update = function(req, res){
+//     if(req.user.id == req.params.id){
+//         User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+//             return res.redirect('back');
+//         });
+//     }else{
+//         return res.status(401).send('Unauthorised');
+//     }
+// }
+
+// in async/await
+module.exports.update = async function(req, res) {
+    try {
+        const userId = req.params.id;
+        const userData = req.body;
+
+        // Check if the current user is the same user being updated
+        if (req.user.id == userId) {
+            // Find the user by ID and update the data
+            let user = await User.findByIdAndUpdate(userId, userData, { new: true }).exec();
+            
+            // Handle the case when the user is not found
+             if (!user) {
+                 return res.redirect('back');
+             }
+
+            return res.redirect('back');
+        } else {
+            return res.status(401).send('Unauthorised');
+        }
+    } catch (err) {
+        console.log('Error in updating user', err);
+        return res.redirect('back');
+    }
+};
+
+// //const User = require('../models/user');
+// const fs = require('fs');
+// //const path = require('path');
+
+// module.exports.update = async function (req, res) {
+//     if (req.user.id == req.params.id) {
+//         try {
+//             let user = await User.findById(req.params.id);
+
+//             // Check if the user exists
+//             if (!user) {
+//                 return res.redirect('back');
+//             }
+
+//             // Update user fields with the values from req.body
+//             user.name = req.body.name;
+//             user.email = req.body.email;
+
+//             // Handle the avatar update if a new file is uploaded
+//             if (req.file) {
+//                 if (user.avatar) {
+//                     fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+//                 }
+//                 user.avatar = User.avatarPath + '\\' + req.file.filename;
+//             }
+
+//             // Save the updated user
+//             await user.save();
+
+//             return res.redirect('back');
+//         } catch (error) {
+//             console.log('Error in updating user', error);
+//             return res.redirect('back');
+//         }
+//     } else {
+//         return res.status(401).send('Unauthorized');
+//     }
+// }
+
+
+
 
 // render the sign up page
 module.exports.signUp = function(req, res){
