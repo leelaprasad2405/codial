63f71e4685f8af8cdda9750aa8dc500b0975eab5
Leela <leelaprasad2405@gmail.com>
2023-08-04 19:40:19 +0530

    successfully done nodews proj

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -42,27 +42,52 @@
 // module.exports = passport;
 
 // above code from cg
-const passport = require('passport');
-const User = require('../models/user');
-const LocalStrategy = require('passport-local').Strategy;
+ const passport = require('passport');
+ const User = require('../models/user');
+ const LocalStrategy = require('passport-local').Strategy;
 
 passport.use(new LocalStrategy({
-    usernameField: 'email'
-}, function (email, password, done) {
+    usernameField: 'email',
+    passReqToCallback: true
+}, function (req, email, password, done) {
     // find a user and establish the identity
     User.findOne({ email: email }).exec() // Use exec() without a callback
         .then(user => {
             if (!user || user.password !== password) {
                 console.log('Invalid username/password');
+                //req.flash('Invalid username/password', err);
                 return done(null, false);
             }
             return done(null, user);
         })
         .catch(err => {
             console.log('Error in finding user ---> passport');
+            //req.flash('error', err);
             return done(err);
         });
 }));
+
+// passport.use(new LocalStrategy({
+//     usernameField: 'email',
+//     passReqToCallback: true
+// },
+//     function (req, email, password, done) {
+//         // find a user and establish the identity
+//         User.findOne({ email: email })
+//             .then(user => {
+//                 if (!user || user.password !== password) {
+//                     //console.log('Invalid username/password');
+//                     req.flash('error', 'Invalid UserName / Password');
+//                     return done(null, false);
+//                 }
+//                 return done(null, user);
+//             })
+//             .catch(err => {
+//                 console.log('Error in finding user ---> passport');
+//                 return done(err);
+//             });
+//     }
+// ));
 
 // serializing the user to decide which key is to be kept in the cookies
 passport.serializeUser(function (user, done) {
